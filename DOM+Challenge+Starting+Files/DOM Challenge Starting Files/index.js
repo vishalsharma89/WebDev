77c12b5dd74e099d8a22dd20da990b42c604bb35
Vishal Sharma <80852001+vishalsharma89@users.noreply.github.com>
2024-01-07 13:25:17 +0530

    Add files via upload
    Bootstrap And JavaScript Basics

---
+++ DOM+Challenge+Starting+Files/DOM Challenge Starting Files/index.js
@@ -0,0 +1,9 @@
+document.getElementsByTagName("li")[2].innerHTML = "Vishal";
+// document.firstElementChild.lastElementChild.lastElementChild.lastElementChild.innerHTML = "Vishal"
+
+
+document.querySelector(".list a").style.color= "red";
+// document.querySelectorAll("li a")[0].style.color="red";
+// document.querySelector("ul").style.backgroundColor = "blue";
+
+document.querySelector("button").style.backgroundColor="yellow";
